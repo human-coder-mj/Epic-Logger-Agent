6e94f271567252647182c7cd502533ecdd84aad4
human-coder-mj <jhamanas4532@gmail.com>
2025-08-27 15:38:50 +0530

    converted to package

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+"""
+Epic Changelog Agent Package
+A theatrical changelog generator that transforms mundane updates into epic narratives.
+"""
+
+from .epic_log_generator import EpicChangelogAgent
+
+__version__ = "1.0.0"
+__author__ = "Epic Changelog Team"
+__all__ = ["EpicChangelogAgent"]
